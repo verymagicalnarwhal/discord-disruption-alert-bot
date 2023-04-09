91ab5a34862c4dfbe2bcc620b217a33b3b8cc844
verymagicalnarwhal <anna.bekhoei@gmail.com>
2023-04-09 22:37:37 +0200

    Added commands and fixed message problems
    Added commands: one that create a log where all discord messages is sent, a command that echoes messages back to you and one that makes the bot say hi to you by username.
    
    Program was fixed with the DiscordSocketConfig including GatewayIntents.AllUnprivileged

---
+++ DiscordBot/InfoModule.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    // Create a module with no prefix
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello world -> hello world
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
+
+        // ReplyAsync is a method on ModuleBase 
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,18 +1,42 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using DiscordBot;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 public class Program
 {
-    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
     private DiscordSocketClient _client;
+    public CommandService _commands;
+    private IServiceProvider _services;
 
+    private SocketGuild _guild;
+
+    private ulong _logChannelID;
+    private SocketTextChannel _logChannel;
 
     public async Task MainAsync()
     {
-        string tokenURL = "C:\\Users\\Anna\\Documents\\GitHub\\discord-disruption-alert-bot\\SecretToken.txt";
 
-        _client = new DiscordSocketClient();
+        string tokenURL = "D:\\Github\\discord-disruption-alert-bot\\SecretToken.txt";
+
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+
+        };
+       
+        _client = new DiscordSocketClient(config);
+        _commands = new CommandService();
+
+        _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
 
         _client.Log += Log;
 
@@ -25,16 +49,81 @@ public class Program
         // var token = File.ReadAllText("token.txt");
         // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
+        await RegisterCommandsAsync();
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
         // Block this task until the program is closed.
         await Task.Delay(-1);
+
     }
 
     private Task Log(LogMessage msg)
     {
         Console.WriteLine(msg.ToString());
         return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    public async Task HandleCommandAsync(SocketMessage arg)
+    {
+        var message = arg as SocketUserMessage;
+        var context = new SocketCommandContext(_client, message);
+        var channel = _client.GetChannel(_logChannelID) as SocketTextChannel;
+
+        //console log with message received and user info
+        Console.WriteLine("-----------\nUser:   " + message.Author.Username + " with ID " + message.Author.Id +
+                                                    "\nHave sent: " +
+                                                    "\n" + message.ToString());
+
+        if (message.Author.IsBot) return;
+
+        int argPos = 0;
+
+        if (message.HasStringPrefix("!", ref argPos))
+        {
+            Console.WriteLine("Prefix");
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync
+                    (result.ErrorReason);
+        }
+
+        var text = message.ToString().ToLower();
+
+        if (text == "/crea log")
+        {
+            ulong messageChannelId = message.Channel.Id;
+            var textChannel = _client.GetChannel(messageChannelId) as SocketTextChannel;
+            _guild = _client.GetGuild(textChannel.Category.GuildId);
+            var newChannel = await _guild.CreateTextChannelAsync("log");
+
+            _logChannelID = newChannel.Id;
+            await textChannel.SendMessageAsync("Channel created - Open it to see the Bot's log.");
+            await newChannel.SendMessageAsync("Channel created - Here you can see the Bot's log.");
+        }
+
+        if (channel !=null)
+        {
+            await channel.SendMessageAsync("-----------\nUser:   " + message.Author.Username + "with ID  " +
+                                           message.Author.Id +
+                                           "\nWrite: " +
+                                           "\n" + message.ToString());
+        }
+
+        switch (text)
+        {
+            case "hi":
+                await message.Channel.SendMessageAsync("Hello " + message.Author.Username + "!");
+                Console.WriteLine("Message Received");
+                break;
+        }
+
+
     }
 }
